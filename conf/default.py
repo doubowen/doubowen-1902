7ff6a593bd5b6ead67a78c8133de693c2824bff2
doubowen <2313324267@qq.com>
2019-04-17 17:24:21 +0800

    ee

--- conf/default.py
+++ conf/default.py
@@ -30,10 +30,10 @@ from django.conf.global_settings import *  # noqa
 # 应用基本信息配置 (请按照说明修改)
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
-APP_ID = 'hzy-demo'
-APP_TOKEN = '541075c8-d862-452d-b35d-63d656044ac2'
+APP_ID = 'dou-test'
+APP_TOKEN = 'a9dda0ac-ca80-45c7-8750-9ba423188a3a'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
-BK_PAAS_HOST = 'https://paas.blueking.com'
+BK_PAAS_HOST = 'http://paas.zorkdata.com'
 BK_PAAS_INNER_HOST = os.environ.get('BK_PAAS_INNER_HOST', BK_PAAS_HOST)
 # 请求官方 API 默认版本号，可选值为："v2" 或 ""；其中，"v2"表示规范化API，""表示未规范化API
 DEFAULT_BK_API_VER = 'v2'
--- conf/settings_development.py
+++ conf/settings_development.py
@@ -19,10 +19,10 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': 'hzy',                        # 数据库名 (默认与APP_ID相同)
+        'NAME': 'doubowen-cui',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
-        'PASSWORD': 'zorkdata.8888',                        # 你的数据库password
-        'HOST': '192.168.1.157',                   		   # 数据库HOST
+        'PASSWORD': 'root',                        # 你的数据库password
+        'HOST': '192.168.1.92',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
